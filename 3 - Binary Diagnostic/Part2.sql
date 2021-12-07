0735431713c88d1aab41c1e5f4a3932fdb3aec7c
Ned Grady <gradyned@gmail.com>
2021-12-07 12:51:13 +0000

    THREE POINT TWO

--- 3 - Binary Diagnostic/Part2.sql
+++ 3 - Binary Diagnostic/Part2.sql
@@ -3,7 +3,7 @@ GO
 
 CREATE TABLE dbo.DiagnosticReportEntry
 (
-	Binary char(12) NOT NULL
+	Binary char(5) NOT NULL
 )
 GO
 
@@ -15,7 +15,7 @@ BULK INSERT dbo.DiagnosticReportEntry  FROM 'C:\Code\advent-of-code-2021\3 - Bin
 GO
 
 DECLARE
-	@NumberOfBits int = 12,
+	@NumberOfBits int = 5,
 	@CurrentBitPosition int = 1,
 	@CurrentGammaRate int = 0,
 	@CurrentEpsilonRate int = 0
