d02aea8c11d58347c2795e14a8e25c606c63b28a
Ned Grady <gradyned@gmail.com>
2023-12-06 02:10:10 +0000

    Never written worse code in my life

--- Advent Of Code/2023/3 Gear Ratios/solution.rb
+++ Advent Of Code/2023/3 Gear Ratios/solution.rb
@@ -76,11 +76,200 @@ class Schematic
   def is_symbol(character)
     character =~ /\D/ && character != '.'
   end
+
+  def sum_of_gear_ratios
+    running_sum_of_gear_ratios = 0
+    for y in 0..@schematic_raw_data.length - 1
+      for x in 0..@schematic_raw_data[y].length - 1
+          running_sum_of_gear_ratios += gear_ratio(x, y)
+      end
+    end
+
+    running_sum_of_gear_ratios
+  
+  end
+
+  def gear_ratio(x, y)
+    # if the current chacater is a '*' AND contains exactly two adjacent numbers
+    # then return the ratio of those two numbers
+    if @schematic_raw_data[y][x] == '*'
+      adjacent_numbers = adjacent_numbers(x, y)
+      if adjacent_numbers.length == 2
+        puts "Found a gear ratio: #{adjacent_numbers[0]} * #{adjacent_numbers[1]} at #{x}, #{y}"
+        return adjacent_numbers[0] * adjacent_numbers[1]
+      end
+    end
+    0
+  end
 end
+
+  def adjacent_numbers(x, y)
+    adjacent_numbers = []
+    
+    # check the row above, left side for a number
+    current_thing_to_check = north_west(x, y)
+    north_west_number_as_string = ''
+    unless current_thing_to_check.nil?
+      # while we're on a number
+      while (current_thing_to_check != nil && current_thing_to_check[:value] =~ /\d/)
+        north_west_number_as_string += current_thing_to_check[:value]
+        current_thing_to_check = west(current_thing_to_check[:x], current_thing_to_check[:y])
+      end
+    end
+
+    north_west_number_as_string = north_west_number_as_string.reverse
+
+    current_thing_to_check = north_east(x, y)
+    north_east_number_as_string = ''
+    unless current_thing_to_check.nil?
+      # while we're on a number
+      while(current_thing_to_check != nil && current_thing_to_check[:value] =~ /\d/)
+        north_east_number_as_string += current_thing_to_check[:value]
+        current_thing_to_check = east(current_thing_to_check[:x], current_thing_to_check[:y])
+      end
+    end
+
+    if(north(x, y)[:value] =~ /\d/)
+      puts 'here'
+      spanning_northern_number_as_string = "#{north_west_number_as_string}#{north(x, y)[:value]}#{north_east_number_as_string}"
+      puts spanning_northern_number_as_string
+      adjacent_numbers.push(spanning_northern_number_as_string.to_i) unless spanning_northern_number_as_string.empty?
+    else
+      adjacent_numbers.push(north_west_number_as_string.to_i) unless north_west_number_as_string.empty?
+      adjacent_numbers.push(north_east_number_as_string.to_i) unless north_east_number_as_string.empty?
+    end
+
+
+    current_thing_to_check = south_west(x, y)
+    south_west_number_as_string = ''
+    unless current_thing_to_check.nil?
+      # while we're on a number
+      while current_thing_to_check != nil && current_thing_to_check[:value] =~ /\d/
+        south_west_number_as_string += current_thing_to_check[:value]
+        current_thing_to_check = west(current_thing_to_check[:x], current_thing_to_check[:y])
+      end
+    end
+
+    south_west_number_as_string = south_west_number_as_string.reverse
+
+    current_thing_to_check = south_east(x, y)
+    south_east_number_as_string = ''
+    unless current_thing_to_check.nil?
+      # while we're on a number
+      while(current_thing_to_check != nil && current_thing_to_check[:value] =~ /\d/)
+        south_east_number_as_string += current_thing_to_check[:value]
+        current_thing_to_check = east(current_thing_to_check[:x], current_thing_to_check[:y])
+      end
+    end
+
+    if(south(x, y)[:value] =~ /\d/)
+      puts 'hehre 2'
+      spanning_southern_number_as_string = "#{south_west_number_as_string}#{south(x, y)[:value]}#{south_east_number_as_string}"
+      puts spanning_southern_number_as_string
+      adjacent_numbers.push(spanning_southern_number_as_string.to_i) unless spanning_southern_number_as_string.empty?
+    else
+      adjacent_numbers.push(south_west_number_as_string.to_i) unless south_west_number_as_string.empty?
+      adjacent_numbers.push(south_east_number_as_string.to_i) unless south_east_number_as_string.empty?
+    end
+
+    puts 'hereee'
+    puts adjacent_numbers.length
+
+
+    current_thing_to_check = west(x, y)
+    west_number_as_string = ''
+    unless current_thing_to_check.nil?
+      # while we're on a number
+      while (current_thing_to_check != nil && current_thing_to_check[:value] =~ /\d/)
+        west_number_as_string += current_thing_to_check[:value]
+        current_thing_to_check = west(current_thing_to_check[:x], current_thing_to_check[:y])
+      end
+    end
+
+    west_number_as_string = west_number_as_string.reverse
+    adjacent_numbers.push(west_number_as_string.to_i) unless west_number_as_string.empty?
+
+    current_thing_to_check = east(x, y)
+    east_number_as_string = ''
+    unless current_thing_to_check.nil?
+      # while we're on a number
+      while(current_thing_to_check != nil && current_thing_to_check[:value] =~ /\d/)
+        east_number_as_string += current_thing_to_check[:value]
+        current_thing_to_check = east(current_thing_to_check[:x], current_thing_to_check[:y])
+      end
+    end
+
+    adjacent_numbers.push(east_number_as_string.to_i) unless east_number_as_string.empty?
+    adjacent_numbers
+  end
+
+  def north_west(x, y)
+    if (x > 0 && y > 0)
+      return { value: @schematic_raw_data[y - 1][x - 1], x: x - 1, y: y - 1 }
+    end
+    return nil
+  end
+
+  def north(x, y)
+    if (y > 0)
+      return { value: @schematic_raw_data[y - 1][x], x: x, y: y - 1 }
+    end
+
+    return nil
+  end
+
+  def west(x, y)
+    if (x > 0)
+      return { value: @schematic_raw_data[y][x - 1], x: x - 1, y: y }
+    end
+
+    return nil
+  end
+
+  def north_east(x, y)
+    if (x < @schematic_raw_data[y].length - 1 && y > 0)
+      return { value: @schematic_raw_data[y - 1][x + 1], x: x + 1, y: y - 1 }
+    end
+
+    return nil
+  end
+
+  def east(x, y)
+    if (x < @schematic_raw_data[y].length - 1)
+      return { value: @schematic_raw_data[y][x + 1], x: x + 1, y: y }
+    end
+
+    return nil
+  end
+
+  def south_west(x, y)
+    if (x > 0 && y < @schematic_raw_data.length - 1)
+      return { value: @schematic_raw_data[y + 1][x - 1], x: x - 1, y: y + 1 }
+    end
+
+    return nil
+  end
+
+  def south(x, y)
+    if (y < @schematic_raw_data.length - 1)
+      return { value: @schematic_raw_data[y + 1][x], x: x, y: y + 1 }
+    end
+
+    return nil
+  end
+
+  def south_east(x, y)
+    if (x < @schematic_raw_data[y].length - 1 && y < @schematic_raw_data.length - 1)
+      return { value: @schematic_raw_data[y + 1][x + 1], x: x + 1, y: y + 1 }
+    end
+
+    return nil
+  end
 
 if __FILE__ == $0
   schematic = Schematic.new (File.readlines './input.txt', chomp: true)
 
   puts schematic.sum_of_part_numbers
+  puts schematic.sum_of_gear_ratios
 
 end
--- Advent Of Code/2023/3 Gear Ratios/solution_spec.rb
+++ Advent Of Code/2023/3 Gear Ratios/solution_spec.rb
@@ -58,4 +58,159 @@ RSpec.describe 'solution' do
 
     expect(schematic.sum_of_part_numbers).to eq(851 + 503 + 832 + 219 + 708 + 534 + 55 + 396)
   end
+
+  it 'Part 2' do
+    schematic_raw_data = [
+      '467..114..',
+      '...*......',
+      '..35..633.',
+      '......#...',
+      '617*......',
+      '.....+.58.',
+      '..592.....',
+      '......755.',
+      '...$.*....',
+      '.664.598..'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(467835)
+  end
+
+  it 'Part north' do
+    schematic_raw_data = [
+      '467.114..',
+      '...*......',
+      '..........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(467 * 114)
+  end
+
+  it 'Full north' do
+    schematic_raw_data = [
+      '4673114..',
+      '...*......',
+      '..........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(0)
+  end
+
+  it 'South' do
+    schematic_raw_data = [
+      '..........',
+      '...*......',
+      '422.111...'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(422 * 111)
+  end
+
+  it 'FULL South' do
+    schematic_raw_data = [
+      '..........',
+      '...*......',
+      '4223111...'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(0)
+  end
+
+  it 'FULL South FULL north' do
+    schematic_raw_data = [
+      '4223111....',
+      '...*.......',
+      '4223111....'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(4223111 * 4223111)
+  end
+
+  it 'East West' do
+    schematic_raw_data = [
+      '..........',
+      '.12*13....',
+      '..........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(12 * 13)
+  end
+
+  it 'One digit numbers' do
+    schematic_raw_data = [
+      '..........',
+      '.2*1...  .',
+      '..........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(2 * 1)
+  end
+
+  it 'One digit numbers' do
+    schematic_raw_data = [
+      '...1......',
+      '..*....  .',
+      '.4........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(4)
+  end
+
+  
+  it 'One digit numbers' do
+    schematic_raw_data = [
+      '.8.8......',
+      '..*....  .',
+      '..........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(8 * 8)
+  end
+
+    
+  it 'One digits side by side' do
+    schematic_raw_data = [
+      '....9*9...',
+      '..........',
+      '..........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(9 * 9)
+  end
+
+  it 'One digits top by bottom', :only do
+    schematic_raw_data = [
+      '..........',  
+      '.....9....',
+      '.....*....',
+      '.....9....',
+      '..........'
+    ]
+
+    schematic = Schematic.new(schematic_raw_data)
+
+    expect(schematic.sum_of_gear_ratios).to eq(9 * 9)
+  end
 end 
